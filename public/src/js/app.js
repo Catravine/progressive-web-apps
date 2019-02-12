cd6d07f76ee349f20f88aa1572991c534ff09540
Caroline Courtney <caroline@carolinecourtney.com>
2019-02-12 18:29:32 -0500

    Fetch & Promises: sample requests, polyfills to compensate in older browsers (so they can all use fetch)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,18 @@
 var deferredPrompt;
 
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
         .then(function() {
             console.log('Service Worker registered!');
         }
-    );
+    ).catch(function(err) {
+        console.log(err)
+    });
 }
 
 window.addEventListener('beforeinstallprompt', function() {
@@ -15,3 +21,36 @@ window.addEventListener('beforeinstallprompt', function() {
     deferredPrompt = event;
     return false;
 });
+
+fetch('https://httpbin.org/ip')
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err)
+    });
+
+
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({message: 'Does this work?'})
+})
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err)
+    });
